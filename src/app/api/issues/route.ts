b41fbf5296c8577deb0323f987f2c3ed74195a4f
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-01-13 19:19:46 +0700

    spinner

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,17 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "../../../../prisma/client";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  des: z.string().min(1),
-});
+import { createIssueSchema } from "../../validattionSchema";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
     data: { title: body.title, des: body.des },
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -3,52 +3,65 @@ import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { Callout } from "@radix-ui/themes";
+import Spinner from "../components/Spinner";
 
 interface IssueForm {
   title: string;
   des: string;
 }
 const NewIssuePage: React.FC = () => {
+  const [isSubmit, setIsSubmit] = useState(false);
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
   return (
-    <form
-      className="max-w-md mx-auto"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <div className="mb-4">
-        <label className="block text-sm font-semibold text-zinc-400 mb-1">
-          Title
-        </label>
-        <input
-          type="text"
-          placeholder="Title"
-          className="p-2 w-full text-sm text-zinc-400 bg-zinc-800 placeholder-zinc-400 border-zinc-600 rounded-md font-semibold"
-          {...register("title")}
-        />
-      </div>
-      <Controller
-        name="des"
-        control={control}
-        render={({ field }) => (
-          <textarea
-            className="p-2 w-full text-sm text-zinc-400 bg-zinc-800 placeholder-zinc-400 border-zinc-600 rounded-md font-semibold h-20"
-            placeholder="Description here..."
-            {...field}
-          ></textarea>
-        )}
-      />
-
-      <button
-        type="submit"
-        className="bg-emerald-500 text-white py-2 px-4 rounded-md hover:bg-emerald-600 focus:outline-none focus:shadow-outline-emerald active:bg-emerald-800"
+    <div>
+      <form
+        className="max-w-md mx-auto"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            setIsSubmit(true);
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error: any) {
+            setIsSubmit(false);
+            console.log(error);
+          }
+        })}
       >
-        Submit
-      </button>
-    </form>
+        <div className="mb-4">
+          <label className="block text-sm font-semibold text-zinc-400 mb-1">
+            Title
+          </label>
+          <input
+            type="text"
+            placeholder="Title"
+            className="p-2 w-full text-sm text-zinc-400 bg-zinc-800 placeholder-zinc-400 border-zinc-600 rounded-md font-semibold"
+            {...register("title")}
+          />
+        </div>
+        <Controller
+          name="des"
+          control={control}
+          render={({ field }) => (
+            <textarea
+              className="p-2 w-full text-sm text-zinc-400 bg-zinc-800 placeholder-zinc-400 border-zinc-600 rounded-md font-semibold h-20"
+              placeholder="Description here..."
+              {...field}
+            ></textarea>
+          )}
+        />
+
+        <button
+          type="submit"
+          disabled={isSubmit}
+          className="bg-emerald-500 text-white py-2 px-4 rounded-md hover:bg-emerald-600 focus:outline-none focus:shadow-outline-emerald active:bg-emerald-800"
+        >
+          Submit
+          {isSubmit && <Spinner />}
+        </button>
+      </form>
+    </div>
   );
 };
 
